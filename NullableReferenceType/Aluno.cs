bcda96f02b751a2a1a1b80f22c43126b71802a97
Fernando Ferreira <fthomm@msn.com>
2019-03-31 19:40:27 -0300

    First commit.

---
+++ NullableReferenceType/Aluno.cs
@@ -0,0 +1,15 @@
+﻿namespace NullableReferenceType
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public int Idade { get; set; }
+
+        public Aluno(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+    }
+}
---
+++ NullableReferenceType/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NullableReferenceType
+{
+    class Program
+    {
+        public static string ExibirDadosAluno(Aluno aluno)
+        {
+            return $"Sobrenome: {aluno.Sobrenome} Nome: {aluno.Nome}, Idade: {aluno.Idade}";
+        }
+
+        public static IEnumerable<string> ListarAlunos(IList<Aluno> alunos)
+        {
+            foreach (var aluno in alunos)
+            {
+                yield return ExibirDadosAluno(aluno);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var alunos = new List<Aluno>()
+            {
+                new Aluno("Fernando", 38 ),
+                new Aluno("Mariana", 32 ),
+                new Aluno("Ana", 29 ),
+            };
+
+            var alunosMaticulados = ListarAlunos(alunos);
+
+            foreach (var aluno in alunosMaticulados)
+            {
+                Console.WriteLine(aluno);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
